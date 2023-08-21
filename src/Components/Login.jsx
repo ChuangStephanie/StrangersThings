4564128db1a62302c80d57f80df0b10d4855ebd6
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-21 09:24:14 -0500

    hooked up login, moved some things around

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,15 @@
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const COHORT_NAME = "2305-FTB-ET-WEB-PT";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
 export default function Authenticate({token}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState ("")
     const [successMessage, setSuccessMessage] = useState(null);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
     async function handleClick() {
         try {
@@ -33,7 +37,15 @@ export default function Authenticate({token}) {
     <h2>Login</h2>
     {successMessage && <p>{successMessage}</p>}
     {error && <p>{error}</p>}
-    <button onClick={handleClick}>Login</button>
+    <form onSubmit={handleClick}>
+            <label>
+                Username: {" "} <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </label>
+            <label>
+                Password: {" "} <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <button onClick={(handleClick) => navigate('./AllPosts')}>Login</button>
+        </form>
     </>
     )
 }
