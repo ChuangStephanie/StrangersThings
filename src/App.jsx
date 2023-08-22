4f4745fdf81acbb7143576034cd2b07ff4a4fbdf
lowl9u <133032114+lowl9u@users.noreply.github.com>
2023-08-22 17:48:19 -0500

    Jarrett updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import AllPosts from './Components/AllPosts';
+import SinglePost from './Components/SinglePost'; // Import the SinglePost component
+import DeletePost from './Components/DeletePost'; // Import the DeletePost component
 import NavBar from './Components/NavBar';
 import Authenticate from './Components/Login';
 
 function App() {
-
   return (
     <>
-    <div className="container">
-      <NavBar />
-    </div>
-    <div className="mainbody">
-      <Routes>
-      <Route path="/" element={<Authenticate />} />
-      <Route path="/AllPosts" element={<AllPosts />} />
-      </Routes>
-    </div>
+      <div className="container">
+        <NavBar />
+      </div>
+      <div className="mainbody">
+        <Routes>
+          <Route path="/" element={<Authenticate />} />
+          <Route path="/AllPosts" element={<AllPosts />} />
+          <Route path="/post/:postId" element={<SinglePost />} />   {/* Add this route */}
+          <Route path="/delete/:postId" element={<DeletePost />} /> {/* Add this route */}
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
