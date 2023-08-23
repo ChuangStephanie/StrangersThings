1997e3f257feefbaa83d1a9ec78c26da716c1063
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-23 10:51:40 -0500

    pushing what I can before trip

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SinglePost from './Components/SinglePost'; // Import the SinglePost compo
 import DeletePost from './Components/DeletePost'; // Import the DeletePost component
 import NavBar from './Components/NavBar';
 import Login from './Components/Login';
+import Profile from './Components/Profile';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AllPosts />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/Profile" element={<Profile />} />
           <Route path="/post/:postId" element={<SinglePost />} />   {/* Add this route */}
           <Route path="/delete/:postId" element={<DeletePost />} /> {/* Add this route */}
         </Routes>
