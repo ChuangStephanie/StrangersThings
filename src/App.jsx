9d4019a856d6f9468b8fcf0d969853d333550256
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-23 11:00:06 -0500

    plz fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import AllPosts from './Components/AllPosts';
 import SinglePost from './Components/SinglePost'; // Import the SinglePost component
 import DeletePost from './Components/DeletePost'; // Import the DeletePost component
 import NavBar from './Components/NavBar';
-
-
+import Authenticate from './Components/Login';
 import Login from './Components/Login';
 import Profile from './Components/Profile';
 
@@ -17,19 +16,17 @@ function App() {
       </div>
       <div className="mainbody">
         <Routes>
-
-
-
-
-
-
-
-          
+        <Route path="/" element={<Authenticate />} />
+          <Route path="/AllPosts" element={<AllPosts />} />
+          <Route path="/post/:postId" element={<SinglePost />} />
+          <Route path="/delete/:postId" element={<DeletePost />} />
+          <Route path="/new-post" element={<NewPost />} /> {/* Add this route */}
+{/* 
           <Route path="/" element={<AllPosts />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/post/:postId" element={<SinglePost />} />   {/* Add this route */}
-          <Route path="/delete/:postId" element={<DeletePost />} /> {/* Add this route */}
+          {/* <Route path="/delete/:postId" element={<DeletePost />} /> Add this route */}
         </Routes>
       </div>
     </>
