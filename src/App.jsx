78c84da22739ce99484b92a16a79058efd550e09
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-23 17:45:41 -0500

    changed around routes and added profile route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import AllPosts from './Components/AllPosts';
-import SinglePost from './Components/SinglePost';
-import DeletePost from './Components/DeletePost';
-import NewPost from './Components/NewPost'; // Import the NewPost component
-import NavBar from './Components/NavBar';
-import Authenticate from './Components/Login';
-import Login from './Components/Login';
-import Profile from './Components/Profile';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import AllPosts from "./Components/AllPosts";
+import SinglePost from "./Components/SinglePost";
+import DeletePost from "./Components/DeletePost";
+import NewPost from "./Components/NewPost"; // Import the NewPost component
+import NavBar from "./Components/NavBar";
+import Authenticate from "./Components/Login";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
@@ -17,11 +17,14 @@ function App() {
       </div>
       <div className="mainbody">
         <Routes>
-          <Route path="/" element={<Authenticate />} />
-          <Route path="/AllPosts" element={<AllPosts />} />
-          <Route path="/post/:postId" element={<SinglePost />} />
-          <Route path="/delete/:postId" element={<DeletePost />} />
-          <Route path="/new-post" element={<NewPost />} /> {/* Add this route */}
+          <Route path="/" element={<AllPosts />} />
+          <Route path="/post/:postId" element={<SinglePost />} />{" "}
+          {/* Add this route */}
+          <Route path="/delete/:postId" element={<DeletePost />} />{" "}
+          {/* Add this route */}
+          <Route path="/new-post" element={<NewPost />} />{" "}
+          {/* Add this route */}
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </>
